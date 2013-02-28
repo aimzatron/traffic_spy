8869e562b68fe2e57534eca477515ebda3954b4c
lalalainexd <lalalainexd@gmail.com>
2013-02-27 22:28:36 -0700

    added more tests for registration. still need to add tests for the descriptions

--- lib/traffic_spy.rb
+++ lib/traffic_spy.rb
@@ -10,6 +10,10 @@ module TrafficSpy
   post '/sources' do
     if params[:identifier].nil? || params[:rootUrl].nil?
       halt(400)
+    elsif !DB.from(:clients).select.where(identifier: params[:identifier]).empty?
+      halt(403)
+    else
+      Client.new params[:identifier], params[:rootUrl]
     end
   end
 
--- spec/traffic_spy_spec.rb
+++ spec/traffic_spy_spec.rb
@@ -10,9 +10,16 @@ end
 describe "Traffic Spy App" do
   include Rack::Test::Methods
 
+  before do
+    #TODO create a method for deleting
+    TrafficSpy::DB["DELETE FROM clients"].delete
+
+    @client_table = TrafficSpy::DB.from(:clients)
+  end
+
   describe "registration" do
 
-    describe "missing params" do
+    context "missing params" do
 
       it "returns a 400 bad request status when missing the identifier" do
         post '/sources', params = {rootUrl:'http://something.com'}
@@ -26,8 +33,28 @@ describe "Traffic Spy App" do
 
     end
 
-    describe "existing identifier" do
+    context "existing identifier" do
+      it "returns a 403 Forbidden status" do
+        TrafficSpy::Client.new('id', 'url')
+        post '/sources', params = {identifier: 'id', rootUrl:'url'}
+        expect(last_response).to be_forbidden
+      end
+    end
 
+    context "acceptable request" do
+      it "returns a 200 OK status" do
+        post '/sources', params = {identifier: 'id', rootUrl:'url'}
+        expect(last_response).to be_ok
+      end
+
+      it "creates a new Client" do
+        post '/sources', params = {identifier: 'id', rootUrl:'url'}
+        puts last_response.inspect
+        #TODO find out why this mock doesn't work
+        #TrafficSpy::Client.should_receive(:new).with('id', 'url')
+        expect(TrafficSpy::DB.from(:clients).count).to eq 1
+
+      end
     end
   end
 end
