fb9cb1f85066767a30384305ea0d1af74810a9ad
Aimee Maher <aimee@geekycheeks.com>
2013-02-28 17:07:27 -0700

    payloads

---
+++ lib/traffic_spy/models/payloads.rb
@@ -0,0 +1,40 @@
+module TrafficSpy
+
+  class Payloads
+    attr_reader :browser_id, 
+                :event_id, 
+                :response_time, 
+                :request_time, 
+                :url_id, 
+                :ip_id, 
+                :screen_resolution_id, 
+                :os_id
+
+    def initialize(params)
+      @browser_id = params[:browser_id]
+      @event_id = params[:event_id]
+      @response_time = params[:response_time]
+      @request_time = params[:request_time]
+      @url_id = params[:url_id]
+      @ip_id = params[:ip_id]
+      @screen_resolution_id = params[:screen_resolution_id]
+      @os_id = params[:os_id]
+    end
+
+    def save
+      Payloads.data.insert(browser_id :browser_id, 
+                            event_id, :event_id, 
+                            response_time :response_time, 
+                            request_time :request_time, 
+                            url_id :url_id, 
+                            ip_id :ip_id, 
+                            screen_resolution_id :screen_resolution_id, 
+                            os_id :os_id)
+    end
+
+    def self.data
+      DB.from :payloads
+    end
+  end
+
+end
---
+++ spec/traffic_spy/models/payloads_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+
+describe TrafficSpy::Payloads do
+
+  before do
+    TrafficSpy::DB["DELETE FROM payloads"].delete
+
+    @payloads_table = TrafficSpy::DB.from(:payloads)
+  end
+
+  describe "new" do
+    it "creates a new entry in the database" do
+
+      payloads = described_class.new(browser_id: 1, 
+                                      event_id: 1, 
+                                      response_time: 1, 
+                                      request_time: 1, 
+                                      url_id: 1, 
+                                      ip_id: 1, 
+                                      screen_resolution_id: 1, 
+                                      os_id: 1)
+
+      expect(payloads.browser_id).to eq 1
+      expect(payloads.event_id).to eq 1
+      expect(payloads.response_time).to eq 1
+      expect(payloads.request_time).to eq 1
+      expect(payloads.url_id).to eq 1
+      expect(payloads.ip_id).to eq 1
+      expect(payloads.screen_resolution_id).to eq 1
+      expect(payloads.os_id).to eq 1
+
+    end
+  end
+
+  describe "instance methods" do
+
+    describe "save" do
+      it "adds an entry to the payloads table" do
+
+        payloads = described_class.new(browser_id: 1, 
+                                      event_id: 1, 
+                                      response_time: 1, 
+                                      request_time: 1, 
+                                      url_id: 1, 
+                                      ip_id: 1, 
+                                      screen_resolution_id: 1, 
+                                      os_id: 1)
+
+        payloads.save
+
+        expect(@payloads_table.count).to eq 1
+
+      end
+    end
+
+  end
+
+end
+
+#:browser_id, :event_id, :response_time, :request_time, :url_id, :ip_id, :screen_resolution_id, :os_id
